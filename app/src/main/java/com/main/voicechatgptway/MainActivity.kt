5d716de125cd815e8b95626096b1913939c07419
vadwwxz <vadwwxz@gmail.com>
2023-04-15 17:03:52 +0300

    Created a logic for speech to text

--- app/src/main/java/com/main/voicechatgptway/MainActivity.kt
+++ app/src/main/java/com/main/voicechatgptway/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.main.voicechatgptway
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.main.voicechatgptway.databinding.ActivityMainBinding
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var launcher by Delegates.notNull<ActivityResultLauncher<Intent>>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                val speechString = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0).toString()
+            }
+        }
+
+        binding.btnSpeak.setOnClickListener {
+            speak()
+        }
+    }
+
+    private fun speak() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking")
+        launcher.launch(intent)
     }
 }
